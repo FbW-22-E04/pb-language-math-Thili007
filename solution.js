1ff95a544d62b6b06496115c65b0ecf1b765da34
Your Name <tpunchihewage@gmail.com>
2022-07-06 16:09:54 +0200

    modified solutions-js

--- solution.js
+++ solution.js
@@ -8,11 +8,11 @@ console.log(Math.max(-1, 4));
 
 // Exercise - 03
 
-console.log(Math.round(3321.32321));
-console.log(Math.round(326.76));
-console.log(Math.round(76788.7));
-console.log(Math.round(-9.78));
-console.log(Math.round(43.342));
+console.log(Math.ceil(3321.32321));
+console.log(Math.ceil(326.76));
+console.log(Math.ceil(76788.7));
+console.log(Math.ceil(-9.78));
+console.log(Math.ceil(43.342));
 
 // Exercise - 04
 
