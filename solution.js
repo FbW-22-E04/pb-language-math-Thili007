498418df151c17d2e4db93073d0d457cdc2f4bfe
Your Name <tpunchihewage@gmail.com>
2022-07-06 16:05:28 +0200

    modified solutions.js

---
+++ solution.js
@@ -0,0 +1,27 @@
+// Exercise - 01
+
+console.log(Math.min(-1, 4));
+
+// Exercise - 02
+
+console.log(Math.max(-1, 4));
+
+// Exercise - 03
+
+console.log(Math.round(3321.32321));
+console.log(Math.round(326.76));
+console.log(Math.round(76788.7));
+console.log(Math.round(-9.78));
+console.log(Math.round(43.342));
+
+// Exercise - 04
+
+console.log(Math.floor(3321.32321));
+console.log(Math.floor(326.76));
+console.log(Math.floor(76788.7));
+console.log(Math.floor(-9.78));
+console.log(Math.floor(28.329));
+
+// Exercise - 05
+
+console.log(Math.floor(Math.random() * 6) + 1);
